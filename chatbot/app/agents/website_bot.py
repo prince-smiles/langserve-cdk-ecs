9356d145befde2ddb26fc555b1f123771714aff4
prince-smiles <prince@doc32.in>
2024-06-21 09:47:49 +0530

    mongo vector search

--- chatbot/app/agents/website_bot.py
+++ chatbot/app/agents/website_bot.py
@@ -1,12 +1,63 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_tool_calling_agent
-from langchain.agents import AgentExecutor
+from langchain.agents import AgentExecutor, Tool
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
 from ..tools.rag import get_treatment_price
 import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from langchain.llms import OpenAI
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.schema import Document
 
+uri = os.environ['MONGO_CONNECTION_STRING']
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client[os.environ['MONGO_DATABASE']]
+embeddings_collection = db['scrapped_data']
+
+def vector_search(query, top_k=5):
+    embedding = OpenAIEmbeddings().embed_documents([query])[0]
+    search_result = embeddings_collection.aggregate([
+        {
+            "$search": {
+                "knnBeta": {
+                    "vector": embedding,
+                    "path": "embedding",
+                    "k": top_k
+                }
+            }
+        }
+    ])
+    documents = []
+    for result in search_result:
+        doc = Document(page_content=result["page_content"], metadata={"url": result["url"]})
+        documents.append(doc)
+    return documents
+
+def qa_chain_tool(query: str) -> str:
+    results = vector_search(query)
+    if results:
+        source_doc = results[0]
+        text = source_doc.page_content
+        url = source_doc.metadata['url']
+        return f"Text: {text}\nURL: {url}"
+    return "No relevant information found"
+
+qa_chain_tool = Tool(
+    name="qa_chain_tool",
+    func=qa_chain_tool,
+    description="Answers questions based on the content of the website"
+)
 
 system_prompt = """
 You are a Dental Assistant named Rahul, People ask you about price of dental treatments and you reply
@@ -26,7 +77,7 @@ prompt = ChatPromptTemplate.from_messages(
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, streaming=True)
 
-tools = [get_treatment_price]
+tools = [ qa_chain_tool ]
 
 agent = create_tool_calling_agent(llm, tools, prompt)
 
